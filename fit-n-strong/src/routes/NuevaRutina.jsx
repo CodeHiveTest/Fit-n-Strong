87004d627c4b66699a64d79f9fdba277a9ee177d
Matías Francia <matiasfrancia1@gmail.com>
2023-05-12 03:39:59 -0400

    Post request

--- fit-n-strong/src/routes/NuevaRutina.jsx
+++ fit-n-strong/src/routes/NuevaRutina.jsx
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Col, Input, FormText } from 'reactstrap';
+import axios from 'axios';
 
 export default function NuevaRutina(props) {
 
+    const [routineName, setRoutineName] = useState('');
+    const [grupo, setGrupo] = useState('');
+
+    function submit(event) {
+        event.preventDefault();
+        console.log("Se mandó");
+        console.log(routineName, " - ", grupo);
+        axios.post('http://20.226.52.146:8080/rutinas', {
+            // nombre: routineName,
+            // grupo_muscular: grupo
+            headers: {
+                "Content-Type": "text/plain",
+            },
+            nombre: routineName,
+            grupo_muscular: grupo
+        })
+        .then(response => console.log(response))
+        .then(data => {
+            if(data.status === 201) {
+                alert("Rutina creada con éxito");
+            }
+        })
+        .catch(error => {
+            alert("Ocurrió el siguiente error al tratar de crear la rutina:", error);
+        });
+
+        // props.toggle();
+    }
+
   return (
-    <Form>
+    <Form onSubmit={submit}>
         <FormGroup row>
             <Label
             for="name"
@@ -18,6 +48,10 @@ export default function NuevaRutina(props) {
                 name="name"
                 placeholder="Ingrese el nombre de la rutina"
                 type="name"
+                value={routineName}
+                onChange={(e) => {
+                  setRoutineName(e.target.value);
+                }}
             />
             </Col>
         </FormGroup>
@@ -34,6 +68,10 @@ export default function NuevaRutina(props) {
                 name="grupo"
                 placeholder="Indique el grupo muscular que pertenece"
                 type="grupo"
+                value={grupo}
+                onChange={(e) => {
+                  setGrupo(e.target.value);
+                }}
             />
             </Col>
         </FormGroup>
@@ -47,7 +85,7 @@ export default function NuevaRutina(props) {
                 size: 10
             }}
             >
-            <Button color="primary" onClick={props.toggle}>
+            <Button color="primary" type='submit'>
                 Crear Rutina
             </Button>{' '}
             <Button color="secondary" onClick={props.toggle}>
--- fit-n-strong/src/routes/Rutinas.jsx
+++ fit-n-strong/src/routes/Rutinas.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useEffect, useState, setState } from "react";
 import { Button, Table, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import EditIcon from '@mui/icons-material/Edit';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import "./../styles/Rutinas.css";
 import NuevaRutina from "./NuevaRutina";
 import EditarRutina from "./EditarRutina";
+import axios from "axios";
 
 export default function Rutinas() {
     const [modalNew, setModalNew] = useState(false);
@@ -17,6 +18,26 @@ export default function Rutinas() {
     const toggleNew = () => setModalNew(!modalNew);
     const toggleEdit = () => setModalEdit(!modalEdit);
     const toggleDelete = () => setModalDelete(!modalDelete);
+
+    const [routines, setRoutines] = useState([]);
+
+    // traer rutinas creadas por el usuario de la API
+    useEffect(() => {
+        getRoutines();
+    }, []);
+
+    function getRoutines() {
+        axios.get('http://20.226.52.146:8080/rutinas', {
+            headers: {
+            "Content-Type": "application/json",
+            },
+        })
+        .then(response => response.data)
+        .then(data => setRoutines([...Object.values(data)]))
+        .catch(error => {
+            console.log("Hubo un error al intentar obtener las rutinas", error);
+        });
+    }
 
     return (
         <div className='container'>
@@ -44,45 +65,47 @@ export default function Rutinas() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th>ID</th>
-                        <td>First Name</td>
-                        <td className='actions'>
-                            <Link to='/ver-rutina'>
-                                <Button color="success">
-                                <RemoveRedEyeIcon /> Ver
-                                </Button>
-                            </Link>
+                    {routines.map((routine, index) => {
+                        return <tr key={index}>
+                            <th>{routine.nombre}</th>
+                            <td>{routine.grupo_muscular}</td>
+                            <td className='actions'>
+                                <Link to='/ver-rutina'>
+                                    <Button color="success">
+                                    <RemoveRedEyeIcon /> Ver
+                                    </Button>
+                                </Link>
 
-                            <Button onClick={toggleEdit} color="warning">
-                                <EditIcon /> Editar
-                            </Button>
-                            <Modal isOpen={modalEdit} fade={false} toggle={toggleEdit}>
-                                <ModalHeader toggle={toggleEdit}>Editar Rutina</ModalHeader>
-                                <ModalBody>
-                                    <EditarRutina props={{toggleDelete}} />
-                                </ModalBody>
-                            </Modal>
-
-                            <Button onClick={toggleDelete} color="danger">
-                                <DeleteIcon /> Eliminar
-                            </Button>
-                            <Modal isOpen={modalDelete} fade={false} toggle={toggleDelete}>
-                                <ModalHeader toggle={toggleDelete}>Eliminar Rutina</ModalHeader>
-                                <ModalBody>
-                                ¿Está seguro que desea eliminar esta rutina? Esta acción no se puede deshacer...
-                                </ModalBody>
-                                <ModalFooter>
-                                <Button color="danger" onClick={toggleDelete}>
-                                    Eliminar
-                                </Button>{' '}
-                                <Button color="secondary" onClick={toggleDelete}>
-                                    Cancelar
+                                <Button onClick={toggleEdit} color="warning">
+                                    <EditIcon /> Editar
                                 </Button>
-                                </ModalFooter>
-                            </Modal>
-                        </td>
-                    </tr>
+                                <Modal isOpen={modalEdit} fade={false} toggle={toggleEdit}>
+                                    <ModalHeader toggle={toggleEdit}>Editar Rutina</ModalHeader>
+                                    <ModalBody>
+                                        <EditarRutina props={{toggleDelete}} />
+                                    </ModalBody>
+                                </Modal>
+
+                                <Button onClick={toggleDelete} color="danger">
+                                    <DeleteIcon /> Eliminar
+                                </Button>
+                                <Modal isOpen={modalDelete} fade={false} toggle={toggleDelete}>
+                                    <ModalHeader toggle={toggleDelete}>Eliminar Rutina</ModalHeader>
+                                    <ModalBody>
+                                    ¿Está seguro que desea eliminar esta rutina? Esta acción no se puede deshacer...
+                                    </ModalBody>
+                                    <ModalFooter>
+                                    <Button color="danger" onClick={toggleDelete}>
+                                        Eliminar
+                                    </Button>{' '}
+                                    <Button color="secondary" onClick={toggleDelete}>
+                                        Cancelar
+                                    </Button>
+                                    </ModalFooter>
+                                </Modal>
+                            </td>
+                        </tr>
+                    }) }
                 </tbody>
             </Table>
         </div>
